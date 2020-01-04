e167bac32b49aaed856f2ba9280494b3adff4b9b
Ivan Coleto <ivancoleto@gmail.com>
2020-01-04 15:41:17 +0100

    yucata script to check name of  player on turn of the first game

--- index.js
+++ index.js
@@ -9,24 +9,58 @@ const password = process.env.PASSWORD;
     browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto(url);
+    await page.goto(url, { waitUntil: 'networkidle0' });
     await page.evaluate((user, password) => {
       document.querySelector('input[title="Login"]').value = user
       document.querySelector('input[title="Password"]').value = password;
       document.querySelector('input[value="Login"]').click();
     }, user, password);
     await page.waitForNavigation();
-
     await page.goto('https://yucata.de/en/CurrentGames', { waitUntil: 'networkidle0' });
-    const gameId = await page.evaluate(() => document.querySelector('#CurrentGames table tbody tr:nth-child(2) td:nth-child(2)').textContent.trim());
-    await page.goto(`https://yucata.de/en/Game/MachiKoro/${gameId}`, { waitUntil: 'networkidle0' })
-    //await page.screenshot({ path: 'gameId-screen.png' });
+    let getCurrentGames = await page.evaluate(() => fetch('https://yucata.de/Services/YucataService.svc/GetCurrentGames',
+      { method: 'POST' })
+      .then((response) => response.json()))
 
-    let playerOnTurn = await page.evaluate(() => document.querySelector('#infoLabel').textContent.replace(/(^.*:)/g, '').replace(' +++', '').trim())
-    console.log(`Player on turn: ${playerOnTurn}`)
+    parseCurrentGames(getCurrentGames).forEach(game => {
+      console.log(`${game.id} ${game.gameName}. It's turn of: ${game.getPlayerOnTurnNickname()} - (Last move on : ${game.lastMoveOn})`)
+    })
+
     await browser.close();
+
   } catch (error) {
     console.log(error)
     await browser.close();
   }
 })()
+
+
+function parseCurrentGames(getCurrentGamesResponse) {
+  const games = [];
+  getCurrentGamesResponse.d.Games.forEach(value => {
+    const game = new Game(value.ID, value.GameName, value.Players, value.PlayerOnTurn, value.LastMoveOn);
+    games.push(game)
+  });
+
+  return games;
+}
+
+class Game {
+  constructor(
+    id,
+    gameName,
+    players = [],
+    playerOnTurn,
+    lastMoveOn,
+  ) {
+    this.id = id,
+    this.gameName = gameName;
+    this.players = players;
+    this.playerOnTurn = playerOnTurn;
+    this.lastMoveOn = lastMoveOn;
+  }
+
+  getPlayerOnTurnNickname() {
+    return this.players.find((player) => player.PlayerID === this.playerOnTurn).Login
+  }
+
+}
